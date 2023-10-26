332d9362f239d4da22e68312cdca7e0cfd90295c
PleahMaCaka <pleahmacaka@gmail.com>
2023-10-26 12:30:47 +0900

    style(example): modify url value and console logs

--- src/Example.ts
+++ src/Example.ts
@@ -1,16 +1,15 @@
 import { Client } from "./Client.js"
 
 const client = new Client({
-    uri: "your uri here", // default by localhost:5000
+    uri: "https://your-share-api-url.trycloudflare.com/api", // default by localhost:5000
 })
 
 client.getCurrentModel().then(model => {
-    console.log(model)
+    console.log(model ?? "No model loaded! :(")
 })
 
 client.chat("Hello Assistant!", {
     character: "Assistant"
 }).then(res => {
-    console.log(`User: ${res.input}`)
-    console.log(`Assistant: ${res.output}`)
+    console.log(`Assistant: ${res}`)
 })
