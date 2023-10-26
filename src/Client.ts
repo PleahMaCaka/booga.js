68a4fb36127c7601ef85308d2db79d9a6387c166
PleahMaCaka <pleahmacaka@gmail.com>
2023-10-26 12:30:37 +0900

    refactor(client): enhance URL initialization and chat method handling

--- src/Client.ts
+++ src/Client.ts
@@ -1,43 +1,38 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
+import axios, { AxiosResponse } from "axios"
 import { defaultChatParams } from "./DefaultParams.js"
 import { ChatParams } from "./types/ChatParams.js"
-import { ChatResult } from "./types/ChatResult.js"
 import { ClientOptions } from "./types/ClientOptions.js"
 
 
 export class Client {
 
-    private readonly _uri: string
+    public readonly url: string
 
     constructor(options: ClientOptions) {
-        this._uri = options.uri ? options.uri + "api/v1/" : "http://127.0.0.1:5000/api/v1/"
+        let url = options.uri ?? "http://127.0.0.1:5000/api/v1/"
 
-        if (!this._uri.endsWith("")) this._uri += "/"
+        if (!url.endsWith("/")) url += "/"
+        if (!url.endsWith("api/")) url += "api/"
+        if (!url.endsWith("v1/")) url += "v1/"
+
+        this.url = url
     }
 
-    async getCurrentModel(): Promise<string> {
-        return axios.get(this._uri + "model").then((res: AxiosResponse) => res.data.result)
+    public async getCurrentModel(): Promise<string | undefined> {
+        const res = await axios.get(this.url + "model").then((res: AxiosResponse) => res.data.result)
+        return (res === "None") ? undefined : res
     }
 
-    async chat(msg: string, params: ChatParams = defaultChatParams, internal: boolean = false): Promise<ChatResult> {
-        const options: AxiosRequestConfig = {
-            method: "POST",
-            url: this._uri + "chat",
-            headers: {
-                "content-type": "application/json"
-            },
-            data: <ChatParams>{ ...params, user_input: msg }
-        }
-
-        return await axios(options).then((res: AxiosResponse) => {
+    public async chat(msg: string, params: ChatParams = defaultChatParams, internal: boolean = false): Promise<string> {
+        return await axios.post(this.url + "chat", <ChatParams>{
+            ...params,
+            user_input: msg
+        }).then((res: AxiosResponse) => {
             // noinspection JSUnresolvedReference
             const history = res.data.results[0].history
             const context = (internal ? history.internal : history.visible).flat()
 
-            return <ChatResult>{
-                input: context[0],
-                output: context[1]
-            }
+            return context[1]
         })
     }
 
